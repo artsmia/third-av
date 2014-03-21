80e1825573ba601e4e226a01d0fcf627d81538c5
Kjell <kjell@leanside.com>
2014-03-21 16:47:49 -0500

    start jennifer's design

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var fs = require('fs')
   , videos = JSON.parse(fs.readFileSync('all.json', 'utf8'))
   , template = require('./videos.jade')
+  , videoList = template({videos: videos})
 
-document.querySelector('#videos').innerHTML = template({videos: videos})
+document.querySelector('#videos #featured .panes').innerHTML = template({videos: videos.slice(3)})
+document.querySelector('#videos #popular').innerHTML = videoList
+document.querySelector('#videos #recent').innerHTML = videoList
+
+
