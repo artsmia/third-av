71b61665c8b8026b61bd4da0b7340aabbe819922
miabot <null+github@artsmia.org>
2014-05-20 18:20:27 -0500

    'Latest', not 'Recent'

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.controller('mainCtrl', ['$scope', '$sce',
   function($scope, $sce) {
     $scope.videos = $scope.recent = vimeoJson.videos
     $scope.albums = vimeoJson.albums
-    $scope.albums.splice(1, 0, {title: "Recent", videos: $scope.recent})
+    $scope.albums.splice(1, 0, {title: "Latest", videos: $scope.recent})
 
     $scope.activateAlbum = function(album) { $scope.activeAlbum = album }
     $scope.activeAlbum = $scope.albums[0]
