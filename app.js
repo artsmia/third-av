1dcb881609ada6902a8f033b09183bfcac723bc6
Tom Borger <thomas.borger@gmail.com>
2014-06-20 13:30:19 -0500

    Very basic URLs for albums

--- app.js
+++ app.js
@@ -12,14 +12,27 @@ app.config(function($sceDelegateProvider) {
   ])
 })
 
-app.controller('mainCtrl', ['$scope', '$sce',
-  function($scope, $sce) {
+app.controller('mainCtrl', ['$scope', '$sce', '$location', 
+  function($scope, $sce, $location) {
     $scope.videos = $scope.recent = vimeoJson.videos
     $scope.albums = vimeoJson.albums
     $scope.albums.splice(1, 0, {title: "Latest", videos: $scope.recent})
 
-    $scope.activateAlbum = function(album) { $scope.activeAlbum = album }
+    $scope.activateAlbum = function( album ) { 
+      $scope.activeAlbum = album;
+      $location.path( album.title );
+    }
+
     $scope.activeAlbum = $scope.albums[0]
+
+    $scope.$on( '$locationChangeSuccess', function(){
+      var path = $location.path().substring(1); // Get rid of starting slash
+      for( var i = 0; i < $scope.albums.length; i++ ) {
+        if( path.toLowerCase() === $scope.albums[i].title.toLowerCase() ){
+          $scope.activateAlbum( $scope.albums[i] );
+        }
+      }
+    });
   }
 ])
 
